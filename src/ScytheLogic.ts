c7f299115d311f6918a20ce28c09dfbdb29cd3c3
JustBenya <justbenya@gmail.com>
2020-12-25 13:14:37 +0700

    Протестировал подсчет очков

--- src/ScytheLogic.ts
+++ src/ScytheLogic.ts
@@ -18,15 +18,24 @@ export const mats = [
     'Фермерский (5)',
 ];
 
+export const structureBonus = [
+    'Количество территорий с туннелями',
+    'Количество озёр',
+    'Количество территорий с символами приключений',
+    'Количество территорий с туннелями и вашими зданиями',
+    'Количество ваших зданий построенных в один ряд',
+    'Количество тундр и ферм с вашими зданиями'
+]
+
 export const calculatePoints = (points: IPoints): number => {
     let result = 0;
 
-    if (points.popularity >= 0 || points.popularity <= 6) {
+    if (points.popularity >= 0 && points.popularity <= 6) {
         result = (points.stars * 3) + (points.territories * 2) + Math.floor(points.resources / 2) + points.buildingBonuses + points.gold;
-    } else if (points.popularity >= 7 || points.popularity <= 12) {
-        result = (points.stars * 4) + (points.territories * 3) + Math.floor(points.resources / 2 * 2) + points.buildingBonuses + points.gold;
-    } else if (points.popularity >= 13 || points.popularity <= 18) {
-        result = (points.stars * 5) + (points.territories * 4) + Math.floor(points.resources / 2 * 3) + points.buildingBonuses + points.gold;
+    } else if (points.popularity >= 7 && points.popularity <= 12) {
+        result = (points.stars * 4) + (points.territories * 3) + Math.floor(points.resources / 2) * 2 + points.buildingBonuses + points.gold;
+    } else if (points.popularity >= 13 && points.popularity <= 18) {
+        result = (points.stars * 5) + (points.territories * 4) + Math.floor(points.resources / 2) * 3 + points.buildingBonuses + points.gold;
     }
 
     return result;
