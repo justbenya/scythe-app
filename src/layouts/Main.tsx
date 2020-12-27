2a68e8317f687230ac54b9b2138ebca86cc597a5
JustBenya <justbenya@gmail.com>
2020-12-27 13:00:41 +0700

    Рефакторинг: немного изменил первый экран

--- src/layouts/Main.tsx
+++ src/layouts/Main.tsx
@@ -1,19 +1,28 @@
+import Container from '@material-ui/core/Container';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import React, { FunctionComponent } from 'react';
 import AppHeader from '../components/AppHeader';
 
-interface OwnProps {}
+interface OwnProps {
+}
 
 type Props = OwnProps;
 
 const Main: FunctionComponent<Props> = (props) => {
 
-  return (
-      <>
-          <CssBaseline />
-          <AppHeader/>
-      </>
-  );
+    return (
+        <>
+            <CssBaseline />
+
+            <AppHeader />
+
+            <Container fixed>
+                <main style={ { height: '90vh', paddingTop: 30 } }>
+                    { props.children }
+                </main>
+            </Container>
+        </>
+    );
 };
 
 export default Main;
--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -1,10 +1,10 @@
 import Button from '@material-ui/core/Button';
-import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import React, { FunctionComponent, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import PlayerRow from '../components/PlayerRow';
 import AppContext from '../context/AppContext';
+import Main from '../layouts/Main';
 import { mats } from '../ScytheLogic';
 
 const Start: FunctionComponent = () => {
@@ -31,35 +31,38 @@ const Start: FunctionComponent = () => {
     };
 
     return (
-        <Container fixed>
-            <main style={ { height: '90vh', paddingTop: 30 } }>
-                <Grid container direction="column" spacing={ 1 }>
+        <Main>
+            <Grid container direction="column" spacing={ 4 }>
+                <Grid item>
+                    <Grid container direction="column" spacing={1}>
+                        <Grid item>
+                            <Button
+                                variant="contained" color="primary" fullWidth
+                                onClick={ createPlayer }
+                            >
+                                Добавить игрока
+                            </Button>
+                        </Grid>
 
-                    <Grid item>
-                        <Button variant="contained" color="primary" fullWidth onClick={ createPlayer }>
-                            Добавить игрока
-                        </Button>
-                    </Grid>
-
-                    <Grid item>
-                        <form>
-                            <Grid container direction="column" spacing={ 4 } style={ { height: '100%' } }>
-                                { Object.values(players).map((player) =>
-                                    <Grid item><PlayerRow key={ player.id } { ...player } /></Grid>) }
-
-                                <Grid item style={ { marginTop: 'auto' } }>
-                                    <Button
-                                        onClick={ handleCalculateScore }
-                                        variant="contained" color="secondary" fullWidth
-                                    >Подсчет очков</Button>
-                                </Grid>
-                            </Grid>
-                        </form>
-
+                        <Grid item>
+                            <Button
+                                variant="contained" color="secondary" fullWidth
+                                onClick={ handleCalculateScore }
+                            >
+                                Подсчет очков
+                            </Button>
+                        </Grid>
                     </Grid>
                 </Grid>
-            </main>
-        </Container>
+
+                <Grid item>
+                    <Grid container direction="column" spacing={ 2 }>
+                        { Object.values(players).map((player) =>
+                            <Grid item><PlayerRow key={ player.id } { ...player } /></Grid>) }
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Main>
     );
 };
 
