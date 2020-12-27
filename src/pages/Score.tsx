76a351ec252f0323b5cf6e09432c51f9dd32704d
JustBenya <justbenya@gmail.com>
2020-12-27 15:03:00 +0700

    Добавил иконки для подсчета очков

--- src/pages/Score.tsx
+++ src/pages/Score.tsx
@@ -1,16 +1,17 @@
-import { Button, Grid, TextField, Typography } from '@material-ui/core';
+import { Button, Grid, InputAdornment, TextField, Typography } from '@material-ui/core';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import HomeIcon from '@material-ui/icons/Home';
+import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
+import StarIcon from '@material-ui/icons/Star';
+import TerrainIcon from '@material-ui/icons/Terrain';
 import React, { FunctionComponent, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import AppContext from '../context/AppContext';
 import Main from '../layouts/Main';
 import { calculatePoints } from '../ScytheLogic';
 
-interface OwnProps {
-}
-
-type Props = OwnProps;
-
-const Score: FunctionComponent<Props> = (props: any) => {
+const Score: FunctionComponent = (props: any) => {
     const { state, fetchPlayers, editPlayer } = React.useContext(AppContext);
 
     useEffect(() => {
@@ -30,6 +31,10 @@ const Score: FunctionComponent<Props> = (props: any) => {
         prevPlayer = currentIndex >= 0 && Object.values(sorted)[currentIndex - 1] ? Object.values(sorted)[currentIndex - 1].id : null;
         nextPlayer = currentIndex >= 0 && Object.values(sorted)[currentIndex + 1] ? Object.values(sorted)[currentIndex + 1].id : null;
     }
+
+    const handleOnFocus = (event: React.FocusEvent<any>): void => {
+        event.target.select()
+    };
 
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         editPlayer({ ...player, [event.target.id]: parseInt(event.target.value) });
@@ -55,106 +60,158 @@ const Score: FunctionComponent<Props> = (props: any) => {
     if (!player) return <div>Loading</div>;
 
     return (
-        <Main title={'Подсчет очков'}>
-            <Typography variant={ 'h5' }>Подсчет очков</Typography>
-            <Typography variant={ 'h6' }>
-                Имя игрока: { player.name }
-            </Typography>
-
-            <form onSubmit={ handleSubmit } autoComplete="off">
-                <Grid container direction={ 'column' } spacing={ 2 }>
-                    <Grid item>
-                        <TextField
-                            id="gold"
-                            fullWidth
-                            variant="outlined"
-                            size="small"
-                            label="Монеты на руках"
-                            type="number"
-                            value={ player.gold }
-                            InputLabelProps={ { shrink: true } }
-                            onChange={ handleInputChange }
-                        />
-                    </Grid>
-
-                    <Grid item>
-                        <TextField
-                            id="popularity"
-                            fullWidth
-                            variant="outlined"
-                            size="small"
-                            label="Популярность"
-                            type="number"
-                            value={ player.popularity }
-                            InputLabelProps={ { shrink: true } }
-                            onChange={ handleInputChange }
-                        />
-                    </Grid>
-
-                    <Grid item>
-                        <TextField
-                            id="stars"
-                            fullWidth
-                            variant="outlined"
-                            size="small"
-                            label="Кол-во звезд"
-                            type="number"
-                            value={ player.stars }
-                            InputLabelProps={ { shrink: true } }
-                            onChange={ handleInputChange }
-                        />
-                    </Grid>
-
-                    <Grid item>
-                        <TextField
-                            id="territories"
-                            fullWidth
-                            variant="outlined"
-                            size="small"
-                            label="Территорий"
-                            type="number"
-                            value={ player.territories }
-                            InputLabelProps={ { shrink: true } }
-                            onChange={ handleInputChange }
-                        />
-                    </Grid>
-
-                    <Grid item>
-                        <TextField
-                            id="resources"
-                            fullWidth
-                            variant="outlined"
-                            size="small"
-                            label="Ресурсы"
-                            type="number"
-                            value={ player.resources }
-                            InputLabelProps={ { shrink: true } }
-                            onChange={ handleInputChange }
-                        />
-                    </Grid>
-
-                    <Grid item>
-                        <TextField
-                            id="buildingBonuses"
-                            fullWidth
-                            variant="outlined"
-                            size="small"
-                            label="Бонусы зданий"
-                            type="number"
-                            value={ player.buildingBonuses }
-                            InputLabelProps={ { shrink: true } }
-                            onChange={ handleInputChange }
-                        />
-                    </Grid>
-
-                    <Grid item>
-                        <Button type="submit" color="primary" variant="contained">Результат</Button>
-                        <Typography>
-                            Очков: { player.points }
-                        </Typography>
-                    </Grid>
+        <Main title={ 'Подсчет очков' }>
+            <Grid container direction={ 'column' } spacing={ 2 }>
+                <Grid item>
+                    <Typography variant={ 'h6' }>Игрок: { player.name }</Typography>
                 </Grid>
-            </form>
+
+                <Grid item>
+                    <form onSubmit={ handleSubmit } autoComplete="off">
+                        <Grid container direction={ 'column' } spacing={ 2 }>
+                            <Grid item>
+                                <TextField
+                                    id="gold"
+                                    fullWidth
+                                    variant="outlined"
+                                    size="small"
+                                    label="Монеты на руках"
+                                    type="number"
+                                    value={ player.gold }
+                                    InputLabelProps={ { shrink: true } }
+                                    onChange={ handleInputChange }
+                                    onFocus={ handleOnFocus }
+                                    InputProps={ {
+                                        startAdornment: (
+                                            <InputAdornment position="start">
+                                                <MonetizationOnIcon />
+                                            </InputAdornment>
+                                        ),
+                                    } }
+                                />
+                            </Grid>
+
+                            <Grid item>
+                                <TextField
+                                    id="popularity"
+                                    fullWidth
+                                    variant="outlined"
+                                    size="small"
+                                    label="Популярность"
+                                    type="number"
+                                    value={ player.popularity }
+                                    InputLabelProps={ { shrink: true } }
+                                    onChange={ handleInputChange }
+                                    onFocus={ handleOnFocus }
+                                    InputProps={ {
+                                        startAdornment: (
+                                            <InputAdornment position="start">
+                                                <FavoriteIcon />
+                                            </InputAdornment>
+                                        ),
+                                    } }
+                                />
+                            </Grid>
+
+                            <Grid item>
+                                <TextField
+                                    id="stars"
+                                    fullWidth
+                                    variant="outlined"
+                                    size="small"
+                                    label="Кол-во звезд"
+                                    type="number"
+                                    value={ player.stars }
+                                    InputLabelProps={ { shrink: true } }
+                                    onChange={ handleInputChange }
+                                    onFocus={ handleOnFocus }
+                                    InputProps={ {
+                                        startAdornment: (
+                                            <InputAdornment position="start">
+                                                <StarIcon />
+                                            </InputAdornment>
+                                        ),
+                                    } }
+                                />
+                            </Grid>
+
+                            <Grid item>
+                                <TextField
+                                    id="territories"
+                                    fullWidth
+                                    variant="outlined"
+                                    size="small"
+                                    label="Территорий (фабрика дает +3)"
+                                    type="number"
+                                    value={ player.territories }
+                                    InputLabelProps={ { shrink: true } }
+                                    onChange={ handleInputChange }
+                                    onFocus={ handleOnFocus }
+                                    InputProps={ {
+                                        startAdornment: (
+                                            <InputAdornment position="start">
+                                                <TerrainIcon />
+                                            </InputAdornment>
+                                        ),
+                                    } }
+                                />
+                            </Grid>
+
+                            <Grid item>
+                                <TextField
+                                    id="resources"
+                                    fullWidth
+                                    variant="outlined"
+                                    size="small"
+                                    label="Всего ресурсов"
+                                    type="number"
+                                    value={ player.resources }
+                                    InputLabelProps={ { shrink: true } }
+                                    onChange={ handleInputChange }
+                                    onFocus={ handleOnFocus }
+                                    InputProps={ {
+                                        startAdornment: (
+                                            <InputAdornment position="start">
+                                                <DonutLargeIcon />
+                                            </InputAdornment>
+                                        ),
+                                    } }
+                                />
+                            </Grid>
+
+                            <Grid item>
+                                <TextField
+                                    id="buildingBonuses"
+                                    fullWidth
+                                    variant="outlined"
+                                    size="small"
+                                    label="Бонусы зданий"
+                                    type="number"
+                                    value={ player.buildingBonuses }
+                                    InputLabelProps={ { shrink: true } }
+                                    onChange={ handleInputChange }
+                                    onFocus={ handleOnFocus }
+                                    InputProps={ {
+                                        startAdornment: (
+                                            <InputAdornment position="start">
+                                                <HomeIcon />
+                                            </InputAdornment>
+                                        ),
+                                    } }
+                                />
+                            </Grid>
+
+                            <Grid item>
+                                <Button type="submit" color="primary" variant="contained">Результат</Button>
+                                <Typography>
+                                    Очков: { player.points }
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </Grid>
+            </Grid>
+
 
             <Button
                 disabled={ !Boolean(prevPlayer) }
