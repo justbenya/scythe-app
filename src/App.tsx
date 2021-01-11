c0127450ad597b5bd44163635a19ded9a241d7f8
JustBenya <justbenya@gmail.com>
2021-01-11 22:21:32 +0700

    Исправил ссылки

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React, { FC } from 'react';
-import { Route, Router, Switch } from 'react-router-dom';
+import React, { FunctionComponent } from 'react';
+import { Redirect, Route, Router, Switch } from 'react-router-dom';
+import history from './history';
 import NotFound from './pages/NotFound';
 import { routes } from './routes';
-import history from './history';
 
-
-const App: FC = () => (
+const App: FunctionComponent = () => (
     <Router history={ history }>
         <Switch>
             { Object.values(routes).map((route: any) => <Route key={ route.path } { ...route } />) }
+            <Route exact path="/"><Redirect to={ routes['index'].path } /></Route>
             <Route component={ NotFound } />
         </Switch>
     </Router>
