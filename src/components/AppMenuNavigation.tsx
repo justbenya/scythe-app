4ecc6770398abe76341bd3d45c2eebe53b6c660c
JustBenya <justbenya@gmail.com>
2021-01-11 22:19:56 +0700

    Исправил ссылки в nav-меню

--- src/components/AppMenuNavigation.tsx
+++ src/components/AppMenuNavigation.tsx
@@ -1,30 +1,44 @@
 import { BottomNavigation, BottomNavigationAction } from '@material-ui/core';
 import React, { FunctionComponent } from 'react';
+import { useSelector } from 'react-redux';
 import { Link, useHistory } from 'react-router-dom';
-import { routes } from '../routes';
+import { getLastAddedFraction } from '../common/scytheLogic';
+import { IPlayer } from '../features/players/types';
+import { RootState } from '../store/rootReducer';
 
 const AppMenuNavigation: FunctionComponent = () => {
     const history = useHistory();
-    const [value, setValue] = React.useState(history.location.pathname);
-
-    const handleChange = (newValue: any) => {
-        setValue(newValue)
-    }
+    const players = useSelector<RootState, IPlayer[]>(state => Object.values(state.players));
 
     return (
-        <BottomNavigation
-            className="menu-footer" value={ value } showLabels
-            onChange={ handleChange }
-        >
-            { Object.values(routes).map(route => (
-                <BottomNavigationAction
-                    key={ route.path }
-                    component={ Link }
-                    label={ route.title }
-                    value={ route.path + value }
-                    to={ route.path }
-                />
-            )) }
+        <BottomNavigation className="menu-footer" value={ history.location.pathname } showLabels>
+            <BottomNavigationAction
+                component={ Link }
+                label="Фракции"
+                value={ `/fraction/${ getLastAddedFraction(players) }` }
+                to={ `/fraction/${ getLastAddedFraction(players) }` }
+            />
+
+            <BottomNavigationAction
+                component={ Link }
+                label="Подсчет очков"
+                value="/score"
+                to="/score"
+            />
+
+            <BottomNavigationAction
+                component={ Link }
+                label="Карта"
+                value="/map"
+                to="/map"
+            />
+
+            <BottomNavigationAction
+                component={ Link }
+                label="Настройки"
+                value="/settings"
+                to="/settings"
+            />
         </BottomNavigation>
     );
 };
